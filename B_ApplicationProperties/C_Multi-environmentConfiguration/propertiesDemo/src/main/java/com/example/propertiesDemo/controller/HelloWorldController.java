f2f49a112a403b77bbb47df44e17329280ee8cca
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-06-28 15:08:50 +0800

    完成多环境配置

---
+++ B_ApplicationProperties/C_Multi-environmentConfiguration/propertiesDemo/src/main/java/com/example/propertiesDemo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.propertiesDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XUAN
+ * @date 2021/6/25 - 16:52
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("HelloWorld")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
+
+}
